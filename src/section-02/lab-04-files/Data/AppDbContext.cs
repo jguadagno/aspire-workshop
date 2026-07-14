a1243b4df04a202fc0d55e803ff0558f7c2079c3
Joseph Guadagno <jguadagno@hotmail.com>
2026-07-14 16:18:41 -0700

    Updated based on flow

---
+++ src/section-02/lab-04-files/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudStore.ProductsApi.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+            entity.Property(p => p.Name).HasMaxLength(200).IsRequired();
+            entity.Property(p => p.Price).HasPrecision(18, 2);
+            entity.Property(p => p.Category).HasMaxLength(100);
+        });
+    }
+}
